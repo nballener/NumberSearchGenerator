aa2d22740a4e681e15449ae54b73b258f60a23b8
Nilo Ballener <nballener@gmail.com>
2019-03-12 13:52:47 +1100

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace numberSearchGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a number search generator!");
+        }
+    }
+}
